f3a70a0d97e9313088344b47928db66eb4163b59
Devanshu Suthar <w0822400@myscc.ca>
2024-09-24 19:29:54 -0400

    Completed Codable Objects

---
+++ SAFinderApp/Models/Accommodation.swift
@@ -0,0 +1,32 @@
+//
+//  Accommodation.swift
+//  SAFinderApp
+//
+//  Created by Devanshu Suthar on 2024-09-24.
+//
+
+import Foundation
+
+enum Section {
+    case main
+}
+
+struct Accommodations: Codable{
+    var results: [Accommodation]
+}
+
+struct Accommodation: Codable, Hashable{
+    var id: Int
+    var image: String?
+    var address: String
+    var rent: Double
+    var phone_number: String
+    var description: String
+    
+    func getImage() -> String? {
+        if image == nil {
+            return nil
+        }
+        return "https://dsuthar.scweb.ca/ios/api.json?\(image!)"
+    }
+}
